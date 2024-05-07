b0d0c32fb2529dfd3b7f949ce9ae13da989ae731
TimothyMendezFeliciano <timothy.mendez@upr.edu>
2024-05-07 11:24:53 -0400

    ownership functions in scope

--- src/ownership_functions.rs
+++ src/ownership_functions.rs
@@ -20,3 +20,22 @@ fn takes_ownership(some_string: String) { // some_string comes into scope
 fn makes_copy(some_integer: i32) { // some_integer comes into scope
     println!("{}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+pub fn returning_ownership() {
+    let s1 = gives_ownership(); //moves its return value into s1
+
+    let s2 = String::from("hello"); // s2 comes into scope
+
+    let s3 = takes_and_gives_back(s2); // s2 is moved into, which also moves its return value into s3.
+} //Here, s3 goes out of scope and is dropped. s2 was moved, so nothing happens. s1 goes out of scope and is dropped.
+
+fn gives_ownership() -> String { // will move its return value into the calling function
+    let some_string = String::from("yours"); // some_string comes into scope
+
+    some_string // returned and moves out to the calling function
+}
+
+// This function takes a String and returns one.
+fn takes_and_gives_back(a_string: String) -> String { // a_string comes into scope
+    a_string // a_string is returned and moves out to the calling function
+}
